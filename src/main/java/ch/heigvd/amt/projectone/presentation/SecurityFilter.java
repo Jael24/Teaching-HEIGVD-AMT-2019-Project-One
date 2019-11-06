5f6d09fa6732fbd102427c488135caea5fd4b680
Jael24 <jael.dubey@heig-vd.ch>
2019-11-06 21:48:35 +0100

    Create security filters

---
+++ src/main/java/ch/heigvd/amt/projectone/presentation/SecurityFilter.java
@@ -0,0 +1,35 @@
+package ch.heigvd.amt.projectone.presentation;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "")
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        if (req.getSession().getAttribute("login") == null || req.getSession().getAttribute("login") == "-1") {
+            resp.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
